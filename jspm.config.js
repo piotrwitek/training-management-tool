404f6cbce7f047bfebf27589ec1932d342adfa5d
Piotr Witek <piotrek.witek@gmail.com>
2016-06-05 01:31:35 +0200

    + added styles  + added typings 1.0 refactor training item

--- jspm.config.js
+++ jspm.config.js
@@ -1,4 +1,25 @@
 SystemJS.config({
+  paths: {
+    "training-app/": "src/"
+  },
+  devConfig: {
+    "map": {
+      "os": "github:jspm/nodelibs-os@0.2.0-alpha",
+      "plugin-typescript": "github:frankwallis/plugin-typescript@4.0.10"
+    },
+    "packages": {
+      "github:frankwallis/plugin-typescript@4.0.10": {
+        "map": {
+          "typescript": "npm:typescript@1.8.10"
+        }
+      },
+      "github:jspm/nodelibs-os@0.2.0-alpha": {
+        "map": {
+          "os-browserify": "npm:os-browserify@0.2.1"
+        }
+      }
+    }
+  },
   transpiler: "plugin-typescript",
   typescriptOptions: {
     "target": "es5",
@@ -30,6 +51,9 @@ SystemJS.config({
   map: {
     "assert": "github:jspm/nodelibs-assert@0.2.0-alpha",
     "buffer": "github:jspm/nodelibs-buffer@0.2.0-alpha",
+    "child_process": "github:jspm/nodelibs-child_process@0.2.0-alpha",
+    "constants": "github:jspm/nodelibs-constants@0.2.0-alpha",
+    "crypto": "github:jspm/nodelibs-crypto@0.2.0-alpha",
     "css": "github:systemjs/plugin-css@0.1.21",
     "domain": "github:jspm/nodelibs-domain@0.2.0-alpha",
     "events": "github:jspm/nodelibs-events@0.2.0-alpha",
@@ -37,9 +61,7 @@ SystemJS.config({
     "http": "github:jspm/nodelibs-http@0.2.0-alpha",
     "https": "github:jspm/nodelibs-https@0.2.0-alpha",
     "mobx": "npm:mobx@2.1.7",
-    "os": "github:jspm/nodelibs-os@0.2.0-alpha",
     "path": "github:jspm/nodelibs-path@0.2.0-alpha",
-    "plugin-typescript": "github:frankwallis/plugin-typescript@4.0.10",
     "process": "github:jspm/nodelibs-process@0.2.0-alpha",
     "react": "npm:react@15.0.2",
     "react-dom": "npm:react-dom@15.0.2",
@@ -47,6 +69,7 @@ SystemJS.config({
     "string_decoder": "github:jspm/nodelibs-string_decoder@0.2.0-alpha",
     "url": "github:jspm/nodelibs-url@0.2.0-alpha",
     "util": "github:jspm/nodelibs-util@0.2.0-alpha",
+    "vm": "github:jspm/nodelibs-vm@0.2.0-alpha",
     "zlib": "github:jspm/nodelibs-zlib@0.2.0-alpha"
   },
   packages: {
@@ -172,14 +195,160 @@ SystemJS.config({
         "querystring": "npm:querystring@0.2.0"
       }
     },
-    "github:frankwallis/plugin-typescript@4.0.10": {
+    "github:jspm/nodelibs-crypto@0.2.0-alpha": {
       "map": {
-        "typescript": "npm:typescript@1.8.10"
+        "crypto-browserify": "npm:crypto-browserify@3.11.0"
       }
     },
-    "github:jspm/nodelibs-os@0.2.0-alpha": {
+    "npm:crypto-browserify@3.11.0": {
       "map": {
-        "os-browserify": "npm:os-browserify@0.2.1"
+        "inherits": "npm:inherits@2.0.1",
+        "browserify-cipher": "npm:browserify-cipher@1.0.0",
+        "browserify-sign": "npm:browserify-sign@4.0.0",
+        "create-hmac": "npm:create-hmac@1.1.4",
+        "create-ecdh": "npm:create-ecdh@4.0.0",
+        "diffie-hellman": "npm:diffie-hellman@5.0.2",
+        "create-hash": "npm:create-hash@1.1.2",
+        "randombytes": "npm:randombytes@2.0.3",
+        "pbkdf2": "npm:pbkdf2@3.0.4",
+        "public-encrypt": "npm:public-encrypt@4.0.0"
+      }
+    },
+    "npm:browserify-sign@4.0.0": {
+      "map": {
+        "inherits": "npm:inherits@2.0.1",
+        "create-hash": "npm:create-hash@1.1.2",
+        "create-hmac": "npm:create-hmac@1.1.4",
+        "parse-asn1": "npm:parse-asn1@5.0.0",
+        "browserify-rsa": "npm:browserify-rsa@4.0.1",
+        "bn.js": "npm:bn.js@4.11.3",
+        "elliptic": "npm:elliptic@6.2.8"
+      }
+    },
+    "npm:create-hmac@1.1.4": {
+      "map": {
+        "inherits": "npm:inherits@2.0.1",
+        "create-hash": "npm:create-hash@1.1.2"
+      }
+    },
+    "npm:pbkdf2@3.0.4": {
+      "map": {
+        "create-hmac": "npm:create-hmac@1.1.4"
+      }
+    },
+    "npm:public-encrypt@4.0.0": {
+      "map": {
+        "randombytes": "npm:randombytes@2.0.3",
+        "create-hash": "npm:create-hash@1.1.2",
+        "parse-asn1": "npm:parse-asn1@5.0.0",
+        "browserify-rsa": "npm:browserify-rsa@4.0.1",
+        "bn.js": "npm:bn.js@4.11.3"
+      }
+    },
+    "npm:create-hash@1.1.2": {
+      "map": {
+        "inherits": "npm:inherits@2.0.1",
+        "cipher-base": "npm:cipher-base@1.0.2",
+        "sha.js": "npm:sha.js@2.4.5",
+        "ripemd160": "npm:ripemd160@1.0.1"
+      }
+    },
+    "npm:diffie-hellman@5.0.2": {
+      "map": {
+        "randombytes": "npm:randombytes@2.0.3",
+        "miller-rabin": "npm:miller-rabin@4.0.0",
+        "bn.js": "npm:bn.js@4.11.3"
+      }
+    },
+    "npm:browserify-cipher@1.0.0": {
+      "map": {
+        "browserify-aes": "npm:browserify-aes@1.0.6",
+        "evp_bytestokey": "npm:evp_bytestokey@1.0.0",
+        "browserify-des": "npm:browserify-des@1.0.0"
+      }
+    },
+    "npm:browserify-aes@1.0.6": {
+      "map": {
+        "create-hash": "npm:create-hash@1.1.2",
+        "inherits": "npm:inherits@2.0.1",
+        "cipher-base": "npm:cipher-base@1.0.2",
+        "evp_bytestokey": "npm:evp_bytestokey@1.0.0",
+        "buffer-xor": "npm:buffer-xor@1.0.3"
+      }
+    },
+    "npm:evp_bytestokey@1.0.0": {
+      "map": {
+        "create-hash": "npm:create-hash@1.1.2"
+      }
+    },
+    "npm:create-ecdh@4.0.0": {
+      "map": {
+        "bn.js": "npm:bn.js@4.11.3",
+        "elliptic": "npm:elliptic@6.2.8"
+      }
+    },
+    "npm:parse-asn1@5.0.0": {
+      "map": {
+        "create-hash": "npm:create-hash@1.1.2",
+        "pbkdf2": "npm:pbkdf2@3.0.4",
+        "browserify-aes": "npm:browserify-aes@1.0.6",
+        "evp_bytestokey": "npm:evp_bytestokey@1.0.0",
+        "asn1.js": "npm:asn1.js@4.6.2"
+      }
+    },
+    "npm:cipher-base@1.0.2": {
+      "map": {
+        "inherits": "npm:inherits@2.0.1"
+      }
+    },
+    "npm:sha.js@2.4.5": {
+      "map": {
+        "inherits": "npm:inherits@2.0.1"
+      }
+    },
+    "npm:miller-rabin@4.0.0": {
+      "map": {
+        "bn.js": "npm:bn.js@4.11.3",
+        "brorand": "npm:brorand@1.0.5"
+      }
+    },
+    "npm:browserify-des@1.0.0": {
+      "map": {
+        "cipher-base": "npm:cipher-base@1.0.2",
+        "inherits": "npm:inherits@2.0.1",
+        "des.js": "npm:des.js@1.0.0"
+      }
+    },
+    "npm:browserify-rsa@4.0.1": {
+      "map": {
+        "randombytes": "npm:randombytes@2.0.3",
+        "bn.js": "npm:bn.js@4.11.3"
+      }
+    },
+    "npm:elliptic@6.2.8": {
+      "map": {
+        "inherits": "npm:inherits@2.0.1",
+        "bn.js": "npm:bn.js@4.11.3",
+        "brorand": "npm:brorand@1.0.5",
+        "hash.js": "npm:hash.js@1.0.3"
+      }
+    },
+    "npm:des.js@1.0.0": {
+      "map": {
+        "inherits": "npm:inherits@2.0.1",
+        "minimalistic-assert": "npm:minimalistic-assert@1.0.0"
+      }
+    },
+    "npm:asn1.js@4.6.2": {
+      "map": {
+        "bn.js": "npm:bn.js@4.11.3",
+        "inherits": "npm:inherits@2.0.1",
+        "minimalistic-assert": "npm:minimalistic-assert@1.0.0"
+      }
+    },
+    "npm:hash.js@1.0.3": {
+      "map": {
+        "inherits": "npm:inherits@2.0.1"
       }
     }
   }
--- src/app-typings.d.ts
+++ src/app-typings.d.ts
@@ -1,3 +1,5 @@
+/// <reference path="../typings/index.d.ts" />
+
 type Todo = {
   uid: number;
   title: string;
