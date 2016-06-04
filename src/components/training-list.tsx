a21780a778407f34f7d7c8a9b08cea408034cde3
Pan Rada Production <sebastian.antolik@gmail.com>
2016-06-04 09:03:26 +0200

    BEM add
    fixx style + change size columns

--- src/components/training-list.tsx
+++ src/components/training-list.tsx
@@ -12,8 +12,8 @@ export class TrainingList extends React.Component<IProps, {}> {
   render() {
     let trainings = this.props.store.state;
     return (
-      <div className="wrapper">
-        <div className="masonry">
+      <div className="traininglist">
+        <div className="traininglist-container">
           { trainings.map((trainingModel) =>
             <TrainingItem key={trainingModel.uid} data={trainingModel} onRemove={this.props.onRemove} />) }
         </div>
@@ -31,14 +31,14 @@ const TrainingItem = (props: TrainingItemProps) => {
   let handleRemove = () => {
     props.onRemove(training.uid);
   }
-  return <div key={training.uid}  className="item">
-    <div className="item-title">{training.title}</div>
+  return <div key={training.uid}  className="traininglist-item">
+    <div className="traininglist-item__title">{training.title}</div>
     <hr/>
-    <div className="item-body">
+    <div className="traininglist-item__body">
       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem dolor eveniet expedita labore laboriosam, molestias mollitia,
     </div>
     <hr/>
-    <div className="item-footer">
+    <div className="traininglist-item__footer">
       <ul>
         <li><a onClick={handleRemove} href="javascript:undefined" >Remove</a></li>
         <li><a onClick="" href="javascript:undefined" >Edit</a></li>
